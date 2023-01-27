ba3374c395cfa2337e5f51164edfc990e68c9f34
CMNisal <cmnisal@gmail.com>
2023-01-27 15:14:12 +0530

    db

---
+++ binance-p2p-bot/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+?>
